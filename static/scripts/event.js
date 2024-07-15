be20c8866c28138a07a7e5840b60c4a5be9d85d4
bulbik <russi0947@gmail.com>
2024-07-15 15:12:44 +0400

    btn_test

--- static/scripts/event.js
+++ static/scripts/event.js
@@ -1,18 +1,6 @@
 let tg = window.Telegram.WebApp;
 
-function chec_sub() {
-    var tg_id = tg.initDataUnsafe.user.id;
-    var tg_data = tg.initDataUnsafe
-    document.write(tg_id);
-    // tg.checkChatMember('-10012382913').then(isMember => {
-    //     if (isMember) {
-    //         document.write('Вы подписаны');
-    //         return;
-    //     }
 
-    //     document.write('Вы не подписаны');
-    // });
-}
 
 let field = document.getElementById('input');
 let field_2 = document.getElementById('output');
@@ -27,3 +15,7 @@ try {
     document.getElementById('input').value = error;
     
 }
+
+document.getElementById('test').addEventListener('click', function() {
+    document.getElementById('output').value = 'ok';
+})
