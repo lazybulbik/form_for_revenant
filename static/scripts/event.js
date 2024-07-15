0ed4096ce73e54967f1125d2c32f7deb77ef12fd
bulbik <russi0947@gmail.com>
2024-07-15 14:21:32 +0400

    test event

--- static/scripts/event.js
+++ static/scripts/event.js
@@ -1,9 +1,9 @@
 let tg = window.Telegram.WebApp;
 
 function chec_sub() {
-    tg.checkChatMember('10012382913').then(isMember => {
-        if (!isMember) {
-            tg.sendMessage('10012382913', 'Вы не являетесь участником группы Тропчики. Обратитесь к администраторам группы, чтобы присоединиться к нам.');
+    tg.checkChatMember('-10012382913').then(isMember => {
+        if (isMember) {
+            document.write('Вы подписаны');
             return;
         }
     });
