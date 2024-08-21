6ac47c634caae666780eb9bea407ad3da2ca7fc0
Bulbik <russi0947@gmail.com>
2024-08-21 23:19:44 +0400

    new design start develop

--- bot_test.py
+++ bot_test.py
@@ -1,6 +1,7 @@
-from database_server import Database
-from config import db_url
+from telebot import TeleBot, types
 
-db = Database(db_url)
+bot = TeleBot('5694054533:AAGVrj58icZPu0L0_3XEVwa3pP1tLO1TGkU')
 
-db.update_data(data={'is_form': 0}, filters={'id': 7107852221}, table='users')
+kb = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text='test', web_app=types.WebAppInfo(url='https://9598-94-25-190-90.ngrok-free.app/test')))
+
+bot.send_message(5061120370, 'test', reply_markup=kb)
